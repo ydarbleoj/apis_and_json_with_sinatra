3a40d96baebc424ac522513d23a1b71e1fc49ca2
ydarbleoj <brady.joel@gmail.com>
2014-01-16 09:28:56 -0800

    final commit

--- omdb.rb
+++ omdb.rb
@@ -18,19 +18,28 @@ end
 post '/result' do
   search_str = params[:movie]
 
-  # Make a request to the omdb api here!
-
-
+  response = Typhoeus.get( "http://www.omdbapi.com/", :params => { :s => search_str } )
+  search_str = response.body
+  result = JSON.parse(search_str)
+  
   # Modify the html output so that a list of movies is provided.
   html_str = "<html><head><title>Movie Search Results</title></head><body><h1>Movie Results</h1>\n<ul>"
-  html_str += "<li>#{search_str}</li></ul></body></html>"
 
+  result["Search"].map do |i| 
+    html_str += "<li><a href=/poster/#{i["imdbID"]}>#{i["Title"]} - #{i["Year"]}</a></li>" 
+  end
+    html_str += "</ul></body></html>"
 end
 
 get '/poster/:imdb' do |imdb_id|
-  # Make another api call here to get the url of the poster.
+
+  # refactor this 
+  response = Typhoeus.get( "www.omdbapi.com", :params => { :i => imdb_id } )
+  result = JSON.parse(response.body)
+
+
   html_str = "<html><head><title>Movie Poster</title></head><body><h1>Movie Poster</h1>\n"
-  html_str = "<h3>#{imdb_id}</h3>"
+  html_str += "<h3><img src= #{result["Poster"]}></h3>"
   html_str += '<br /><a href="/">New Search</a></body></html>'
 
 end
